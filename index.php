904d6543c07fe3c46f2e0e7ca677abe1bedf6056
kotosanagi <kotosanagi@gmail.com>
2020-10-29 16:57:15 +0900

    check res and card_array 9

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 define("DB_HOST",$url["host"]);
 define("DB_USER",$url["user"]);
 define("DB_PASS",$url["pass"]);
-define("DB_NAME",substr($url["path"]),1);
+define("DB_NAME",substr($url["path"],1));
 
 session_start();
 //変数の初期化
